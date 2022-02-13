5f280a27442c3cc5e582343cdb9845f69782e27a
Maya Copeland <maya.copeland@protonmail.com>
2022-02-13 19:21:08 +0000

    finished*
    awful, awful code. but it /works/

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # wordle
 Recreation of the popular word-guessing game 'wordle' but in haskell.
+I'm not the best at haskell and i'd love to go back and improve it more and more as time goes on.
--- app/Main.hs
+++ app/Main.hs
@@ -4,18 +4,22 @@ import System.IO
 import Control.Monad (replicateM)
 import Words 
 
-handleGuesses :: Int -> IO()
-handleGuesses 0 = return () 
-handleGuesses n = do 
+handleGuesses :: Int -> String -> IO()
+handleGuesses 0 word = putStrLn $ concat ["Word was ", word]
+handleGuesses n word = do 
     guess <- getLine 
     if guess `elem` Words.wordList then
-        handleGuesses (n-1)
+        if guess == word then
+            putStrLn "Correct word"
+        else 
+            Words.getCharEqual (zip guess word) word >> handleGuesses (n-1) word
     else 
-        putStrLn "Word is invalid" >> handleGuesses (n)
+        putStrLn "Word is invalid" >> handleGuesses (n) word
 
 main :: IO ()
 main = do 
     number <- randomRIO(1,12972::Int)
     let word = (Words.getWords number Words.wordList)
     let guesses = 6
-    handleGuesses guesses
+    putStrLn "======="
+    handleGuesses guesses word
