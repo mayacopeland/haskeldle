981993a9adf6782bbf541942d3bece03e7bf916b
Maya Copeland <maya.copeland@protonmail.com>
2022-02-13 20:23:12 +0000

    use the full word list

--- app/Main.hs
+++ app/Main.hs
@@ -4,22 +4,24 @@ import System.IO
 import Control.Monad (replicateM)
 import Words 
 
-handleGuesses :: Int -> String -> IO()
-handleGuesses 0 word = putStrLn $ concat ["Word was ", word]
-handleGuesses n word = do 
+handleGuesses :: Int -> String -> [String] -> IO()
+handleGuesses 0 word wList = putStrLn $ concat ["Word was ", word]
+handleGuesses n word wList = do 
     guess <- getLine 
-    if guess `elem` Words.wordList then
+    if guess `elem` wList then
         if guess == word then
             putStrLn "Correct word"
         else 
-            Words.getCharEqual (zip guess word) word >> handleGuesses (n-1) word
+            Words.getCharEqual (zip guess word) word >> handleGuesses (n-1) word wList
     else 
-        putStrLn "Word is invalid" >> handleGuesses (n) word
+        putStrLn "Word is invalid" >> handleGuesses (n) word wList
 
 main :: IO ()
 main = do 
-    number <- randomRIO(1,12972::Int)
-    let word = (Words.getWords number Words.wordList)
+    words <- readFile "words.txt"
+    let wList = Words.wordList words
+    number <- randomRIO(1,(length wList)::Int)
+    let word = (Words.getWords number wList)
     let guesses = 6
     putStrLn "======="
-    handleGuesses guesses word
+    handleGuesses guesses word wList
